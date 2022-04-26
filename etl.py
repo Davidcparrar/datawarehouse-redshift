3ee732f727deab4f060689ab0cbb1cfa619b5e23
David Parra <davidc.parrar@gmail.com>
2022-04-26 18:43:14 -0500

    Staging tables uploaded

--- etl.py
+++ etl.py
@@ -16,16 +16,27 @@ def insert_tables(cur, conn):
 
 
 def main():
+    import traceback
+    import sys
+
+    """Load staing tables and insert data into star schema"""
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+    try:
+        conn = psycopg2.connect(
+            "host={} dbname={} user={} password={} port={}".format(
+                *config["CLUSTER"].values()
+            )
+        )
+        cur = conn.cursor()
 
-    conn.close()
+        # load_staging_tables(cur, conn)
+        insert_tables(cur, conn)
+    except Exception:
+        traceback.print_exception(*sys.exc_info())
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
